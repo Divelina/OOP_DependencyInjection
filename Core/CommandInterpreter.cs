8c4e65ee609a098299723c2ee33545f6c859394e
Ivelina Dimitrova <divelina@phys.uni-sofia.bg>
2018-04-06 17:15:27 +0300

    Added an alternative solution for getting the constructor parameters from the serviceProvider.

--- Core/CommandInterpreter.cs
+++ Core/CommandInterpreter.cs
@@ -25,14 +25,25 @@ public class CommandInterpreter : ICommandInterpreter
             throw new InvalidOperationException("Invalid command");
         }
 
-        var fields = commandClass.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
-            .Where(f => f.CustomAttributes.Any(a => a.AttributeType == typeof(InjectAttribute)))
-            .ToArray();
+        //Testing alternative solution without "InjectAttribute" - see bellow
+        //var fields = commandClass.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+        //    .Where(f => f.CustomAttributes.Any(a => a.AttributeType == typeof(InjectAttribute)))
+        //    .ToArray();
 
-        object[] fieldsToInject = fields
-            .Select(f => this.serviceProvider.GetService(f.FieldType)).ToArray();
+        //object[] fieldsToInject = fields
+        //    .Select(f => this.serviceProvider.GetService(f.FieldType)).ToArray();
 
-        object[] commandArguments = fieldsToInject.Concat(new object[] { argugments }).ToArray();
+        //object[] commandArguments = fieldsToInject.Concat(new object[] { argugments }).ToArray();
+
+        //alternative solutioin
+        var constructorParams = commandClass.GetConstructors().First().GetParameters();
+
+        var constructorParamsType = constructorParams.Select(p => p.ParameterType).ToArray();
+
+        var serviceParams = constructorParamsType
+            .Select(serviceProvider.GetService).Where(sp => sp != null).ToArray();
+
+        object[] commandArguments = serviceParams.Concat(new object[] { argugments }).ToArray();
 
         var command = (ICommand) Activator.CreateInstance(commandClass, commandArguments);
 
